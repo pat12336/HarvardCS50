f280aa4f4b4daebad9df0ee786b54d2821af73b4
Patrick Hanna <patrickmhanna92@gmail.com>
2022-04-21 00:16:18 -0700

    finished 3 functions for tictactoe

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,21 +23,41 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x_moves = 0
+    y_moves = 0
+    for row in board:
+        for j in row:
+            if j == X:
+                x_moves += 1
+            elif j == O:
+                y_moves += 1
+    
+    return O if y_moves < x_moves else X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = []
+    for row in range(0,3):
+        for cell in range(0,3):
+            if board[row][cell] == EMPTY:
+                possible_actions.append((row, cell))
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    p = player(board)
+    result_board = copy.deepcopy(board)
+    if result_board[action[0]][action[1]] is not EMPTY:
+        raise Exception("Space is already occupied!")
+
+    result_board[action[0]][action[1]] = p
+    return result_board
 
 
 def winner(board):
